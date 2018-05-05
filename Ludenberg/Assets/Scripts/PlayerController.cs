a713498e520b4514222ecb347c57c32dc41b3dee
Glenn Wilkie-Sullivan <glennpws@hotmail.co.uk>
2018-05-05 19:07:14 +0100

    Fixed jitter on collisions, but need to lock input.

--- Ludenberg/Assets/Scripts/PlayerController.cs
+++ Ludenberg/Assets/Scripts/PlayerController.cs
@@ -9,12 +9,14 @@ public class PlayerController : MonoBehaviour
     public float _speed = 3.0f;
 
     private Animator _anim;
+    private Rigidbody2D _playerBody;
     private bool _playerMoving;
     private Vector2 _lastMove;
 
     void Start()
     {
         _anim = GetComponent<Animator>();
+        _playerBody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -23,14 +25,16 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetAxisRaw("Horizontal") >= 0.5f || Input.GetAxisRaw("Horizontal") <= -0.5f)
         {
-            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * _speed * Time.deltaTime, 0.0f, 0.0f));
+            //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * _speed * Time.deltaTime, 0.0f, 0.0f));
+            _playerBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * _speed, _playerBody.velocity.y);
             _playerMoving = true;
             _lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0.0f);
         }
 
         if (Input.GetAxisRaw("Vertical") >= 0.5f || Input.GetAxisRaw("Vertical") <= -0.5f)
         {
-            transform.Translate(new Vector3(0.0f, Input.GetAxisRaw("Vertical") * _speed * Time.deltaTime, 0.0f));
+            //transform.Translate(new Vector3(0.0f, Input.GetAxisRaw("Vertical") * _speed * Time.deltaTime, 0.0f));
+            _playerBody.velocity = new Vector2(_playerBody.velocity.x, Input.GetAxisRaw("Vertical") * _speed);
             _playerMoving = true;
             _lastMove = new Vector2(0.0f, Input.GetAxisRaw("Vertical"));
         }
