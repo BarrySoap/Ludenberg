2efe54b18dcb179b92325ec5999075dcc81f0ff9
Glenn Sullivan <glennpws@hotmail.co.uk>
2018-11-14 18:08:54 +0000

    Work on player states (turn based combat).

---
+++ Ludenberg/Assets/Scripts/Combat/CombatStates.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatStates : MonoBehaviour
+{
+    public enum BattleStates
+    {
+        START,
+        PLAYERCHOICE,
+        ENEMYCHOICE,
+        LOSE,
+        WIN
+    }
+
+    private BattleStates currentState;
+    
+	void Start ()
+    {
+        currentState = BattleStates.START;
+	}
+	
+	void Update ()
+    {
+		switch (currentState)
+        {
+            case BattleStates.START:
+                break;
+            case BattleStates.PLAYERCHOICE:
+                break;
+            case BattleStates.ENEMYCHOICE:
+                break;
+            case BattleStates.LOSE:
+                break;
+            case BattleStates.WIN:
+                break;
+        }
+	}
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("NEXT STATE:"))
+        {
+            currentState = (BattleStates)(((int)currentState + 1) % 5);
+        }
+    }
+}
