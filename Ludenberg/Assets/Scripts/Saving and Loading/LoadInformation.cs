1e3ba4d447ea12aa01b139690239569d098f39f2
Glenn <glennpws@hotmail.co.uk>
2018-11-17 16:04:46 +0000

    Up to date.

---
+++ Ludenberg/Assets/Scripts/Saving and Loading/LoadInformation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadInformation : MonoBehaviour
+{
+	public static void LoadAllInformation()
+    {
+        GameInformation.PlayerName = PlayerPrefs.GetString("PLAYERNAME");
+
+    }
+}
---
+++ Ludenberg/Assets/Scripts/Saving and Loading/SaveInformation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveInformation
+{
+    public static void SaveAllInformation()
+    {
+        PlayerPrefs.SetString("PLAYERNAME", GameInformation.PlayerName);
+        PlayerPrefs.SetInt("PLAYERLEVEL", GameInformation.PlayerLevel);
+        PlayerPrefs.SetInt("STAMINA", GameInformation.Stamina);
+        PlayerPrefs.SetInt("ENDURANCE", GameInformation.Endurance);
+        PlayerPrefs.SetInt("INTELLECT", GameInformation.Intellect);
+        PlayerPrefs.SetInt("STRENGTH", GameInformation.Strength);
+    }
+}
